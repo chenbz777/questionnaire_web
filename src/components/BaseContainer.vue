f3c4e60df70504a85c5ec226dee3f04e7c88b770
chenbz <user@chen.bz>
2024-10-28 14:17:27 +0800

    feat: 新增布局控件

---
+++ src/components/BaseContainer.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bc">
+    <div class="bc__head">
+      <slot name="head" />
+    </div>
+    <div class="bc__content">
+      <slot />
+    </div>
+    <div class="bc__foot">
+      <slot name="foot" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bc {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+
+  padding-left: constant(safe-area-inset-left);
+  padding-left: env(safe-area-inset-left);
+
+  padding-right: constant(safe-area-inset-right);
+  padding-right: env(safe-area-inset-right);
+
+  padding-top: constant(safe-area-inset-top);
+  padding-top: env(safe-area-inset-top);
+}
+
+.bc__head {
+  box-sizing: border-box;
+  position: relative;
+}
+
+.bc__content {
+  flex: 1;
+  height: 0;
+  overflow: auto;
+  position: relative;
+}
+
+.bc__foot {
+  box-sizing: border-box;
+  position: relative;
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,41 +2,35 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import Vant from 'vant';
-// 2. 引入组件样式
 import 'vant/lib/index.css';
-
-// 路由
-import router from './router';
-
-// element-puls 库
-import ElementPlus from 'element-plus';
+import router from './router';  // 路由
+import ElementPlus from 'element-plus';  // element-puls 库
 import 'element-plus/dist/index.css';
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-
-// 引入animate动画样式
-import 'animate.css';
-
-// 引入全局组件
-import AnimateTransition from '@/components/AnimateTransition.vue';
-import AnimateTransitionGroup from '@/components/AnimateTransitionGroup.vue';
+import 'animate.css';  // 引入animate动画样式
 
 const pinia = createPinia();
 
 const app = createApp(App);
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
-}
-
-app.component('AnimateTransition', AnimateTransition);
-app.component('AnimateTransitionGroup', AnimateTransitionGroup);
 
 app.use(pinia);
 app.use(router);
 app.use(ElementPlus, {
   locale: zhCn
 });
-
 app.use(Vant);
+
+// 自动导入并注册 `components` 文件夹下的所有 `.vue` 文件
+const components = import.meta.glob('@/components/*.vue', { eager: true });
+
+Object.entries(components).forEach(([path, component]) => {
+  const componentName = path.split('/').pop().replace(/\.\w+$/, ''); // 提取文件名作为组件名
+  app.component(componentName, component.default);
+});
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.mount('#app');
