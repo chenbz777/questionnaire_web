637c4aa45aeb1d08fbbbf8aa123e5f5de3c018d2
胡小五 <chenbz@huxiaowudeMac-mini.local>
2024-12-04 15:50:16 +0800

    refactor: 重构签名题目

--- src/hooks/useQuestionnaire/materielComponents/AdvancedSignature.vue
+++ src/hooks/useQuestionnaire/materielComponents/AdvancedSignature.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { toRefs, onMounted } from 'vue';
+import { ref, toRefs, onMounted } from 'vue';
+import { VueSignaturePad } from 'vue-signature-pad';
 
 
 const props = defineProps({
@@ -15,6 +16,8 @@ const props = defineProps({
 
 const { data: componentData } = toRefs(props);
 
+const signaturePadRef = ref(null);
+
 onMounted(() => {
   // 发送生命周期事件
   props.emitSubscribe('onMounted');
@@ -26,9 +29,11 @@ function handleChange(value) {
 }
 
 // 提交事件
-function onSubmit(data) {
+function onSubmit() {
 
-  const { image } = data;
+  const signatureData = signaturePadRef.value.saveSignature();
+
+  const image = signatureData.data || '';
 
   componentData.value.props.defaultValue = image;
 
@@ -37,11 +42,11 @@ function onSubmit(data) {
 }
 
 // 取消事件
-function onClear(value) {
+function onClear() {
 
-  if (!value) {
-    value = '';
-  }
+  signaturePadRef.value.clearSignature();
+
+  const value = '';
 
   componentData.value.props.defaultValue = value;
 
@@ -49,15 +54,55 @@ function onClear(value) {
   handleChange(value);
 }
 
+// 撤销
+function onUndo() {
+  signaturePadRef.value.undoSignature();
+}
+
 const root = getComputedStyle(document.documentElement);
 
 const bgColor = root.getPropertyValue('--questionnaire-content-bg-color').trim();
 
 const color = root.getPropertyValue('--questionnaire-text-color').trim();
+
+const options = {
+  backgroundColor: bgColor,
+  penColor: color
+};
 </script>
 
 <template>
-  <van-signature @submit="onSubmit" @clear="onClear" :background-color="bgColor" :pen-color="color" />
+  <div class="advanced-signature">
+    <div class="advanced-signature__signature">
+      <VueSignaturePad height="200px" :options="options" ref="signaturePadRef" />
+    </div>
+
+    <div class="advanced-signature__btn mr-2" @click="onUndo()">撤销</div>
+    <div class="advanced-signature__btn mr-2" @click="onClear()">清空</div>
+    <div class="advanced-signature__btn advanced-signature__btn--fill" @click="onSubmit()">确定</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.advanced-signature__signature {
+  border: 1px dotted #dadada;
+  border-radius: 10px;
+}
+
+.advanced-signature__btn {
+  display: inline-block;
+  margin-top: 10px;
+  padding: 6px 30px;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 1px solid var(--questionnaire-btn-bg-color);
+  color: var(--questionnaire-btn-bg-color);
+  text-align: center;
+  font-size: 14px;
+}
+
+.advanced-signature__btn--fill {
+  background-color: var(--questionnaire-btn-bg-color);
+  color: var(--questionnaire-btn-text-color);
+}
+</style>
